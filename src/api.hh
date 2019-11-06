7b85fb79653aa938621dba36db0205475cbb74fd
Stefan Westerfeld <stefan@space.twc.de>
2019-11-06 17:06:49 +0100

    Add basic doxygen docs.
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- src/api.hh
+++ src/api.hh
@@ -35,9 +35,40 @@ public:
   Synth();
   ~Synth();
 
+  /**
+   * \brief Set sample rate of the synthesizer
+   *
+   * @param sample_rate   new sample rate
+   */
   void set_sample_rate (uint sample_rate);
+
+  /**
+   * \brief Load .sfz file including all samples
+   *
+   * @param filename   name of the .sfz file
+   */
   bool load (const std::string& filename);
+
+  /**
+   * \brief Add a new midi event to the event list for the next process() call
+   *
+   * Events must be added in the correct order, sorted by offset. The events
+   * will be processed during the next process() call.
+   *
+   * @param offset      time index for the midi event
+   * @param midi_data   raw midi data (for instance {0x90, 60, 100} for note on, middle C, velocity 100)
+   */
   void add_midi_event (uint offset, unsigned char *midi_data);
+
+  /**
+   * \brief Synthesize audio
+   *
+   * This will render one stereo output stream, left output to outputs[0] and
+   * right output to outputs[1]. The contents of the output buffers will be overwritten.
+   *
+   * @param outputs    buffers for the output of the synthesizer
+   * @param n_frames   number of stereo frames to be written
+   */
   void process (float **outputs, uint nframes);
 };
 
