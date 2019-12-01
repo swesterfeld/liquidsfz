31b012e582ddbc89242e25f76279f22eb675b795
Stefan Westerfeld <stefan@space.twc.de>
2019-12-01 16:11:04 +0100

    BUILD: support fully static builds with static libstdc++/libgcc
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- static/build.sh
+++ static/build.sh
@@ -2,4 +2,4 @@
 set -e
 cd ..
 docker build -t liquidsfz -f static/Dockerfile .
-docker run -v $PWD/static:/data -t liquidsfz tar Cczfv /usr/local /data/liquidsfz-static.tar.gz liquidsfz
+docker run -v $PWD/static:/data -t liquidsfz tar Cczfv /usr/local/liquidsfz/lib/lv2 /data/liquidsfz-static.tar.gz liquidsfz.lv2
