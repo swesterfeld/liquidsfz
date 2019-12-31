f8788857c0115689818b3390ce224f78d78c5989
Stefan Westerfeld <stefan@space.twc.de>
2019-12-31 17:44:35 +0100

    SRC: update xfade test
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- src/testxf.cc
+++ src/testxf.cc
@@ -20,16 +20,20 @@
 
 #include "voice.hh"
 
+using namespace LiquidSFZInternal;
+
 int
 main (int argc, char **argv)
 {
-  LiquidSFZInternal::Voice voice (nullptr);
+  Voice voice (nullptr);
 
   int lo = atoi (argv[1]);
   int hi = atoi (argv[2]);
 
   for (int value = lo - 2; value <= hi + 2; value++)
     {
-      printf ("%d %f %f\n", value, voice.xfin_gain (value, lo, hi), voice.xfout_gain (value, lo, hi));
+      printf ("%d %f %f %f %f\n", value,
+              voice.xfin_gain (value, lo, hi, XFCurve::POWER), voice.xfout_gain (value, lo, hi, XFCurve::POWER),
+              voice.xfin_gain (value, lo, hi, XFCurve::GAIN), voice.xfout_gain (value, lo, hi, XFCurve::GAIN));
     }
 }
