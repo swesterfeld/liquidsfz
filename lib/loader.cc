f43dfeb263b315d4ea400a4b63f294a5d3a951fe
Stefan Westerfeld <stefan@space.twc.de>
2020-01-09 18:06:52 +0100

    LIB: add another tune_onccN alias
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- lib/loader.cc
+++ lib/loader.cc
@@ -317,8 +317,9 @@ Loader::set_key_value (const string& key, const string& value)
       region.amplitude_cc.value = convert_float (value);
       update_cc_info (sub_key);
     }
-  else if (split_sub_key (key, "tune_on", sub_key) /* sforzando supports both variants */
-       ||  split_sub_key (key, "tune_oncc", sub_key))
+  else if (split_sub_key (key, "tune_cc", sub_key) /* sforzando supports 3 variants */
+       ||  split_sub_key (key, "tune_oncc", sub_key)
+       ||  split_sub_key (key, "pitch_oncc", sub_key))
     {
       region.tune_cc.cc = sub_key;
       region.tune_cc.value = convert_float (value);
