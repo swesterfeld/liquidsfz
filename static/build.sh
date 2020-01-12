782dd389b213301316568cbc7f71f3e0df25417e
Stefan Westerfeld <stefan@space.twc.de>
2020-01-12 18:11:41 +0100

    BUILD: include package version in lv2 binary tarball
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- static/build.sh
+++ static/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+source ./config.sh
 cd ..
 docker build -t liquidsfz -f static/Dockerfile .
-docker run -v $PWD/static:/data -t liquidsfz tar Cczfv /usr/local/liquidsfz/lib/lv2 /data/liquidsfz-static.tar.gz liquidsfz.lv2
+docker run -v $PWD/static:/data -t liquidsfz tar Cczfv /usr/local/liquidsfz/lib/lv2 /data/liquidsfz-$PACKAGE_VERSION-x86_64.tar.gz liquidsfz.lv2
