922ac5608f1c42821a95bf98dfce1fa9ab44eaa5
Stefan Westerfeld <stefan@space.twc.de>
2020-12-25 12:04:35 +0100

    LIB: implement 2 pole lowpass filter
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- lib/filter.hh
+++ lib/filter.hh
@@ -33,7 +33,8 @@ public:
   enum class Type {
     NONE,
     LPF_1P,
-    HPF_1P
+    HPF_1P,
+    LPF_2P
   };
   static Type
   type_from_string (const std::string& s)
@@ -44,17 +45,45 @@ public:
     if (s == "hpf_1p")
       return Type::HPF_1P;
 
+    if (s == "lpf_2p")
+      return Type::LPF_2P;
+
     return Type::NONE;
   }
 private:
+  /* 1 pole */
   float p = 0;
+
   float tmp_l = 0;
   float tmp_r = 0;
+
+  /* biquad */
+  float a1 = 0;
+  float a2 = 0;
+  float b0 = 0;
+  float b1 = 0;
+  float b2 = 0;
+
+  struct BiquadState
+  {
+    float x1 = 0;
+    float x2 = 0;
+    float y1 = 0;
+    float y2 = 0;
+  } b_state_l, b_state_r;
 
   Type  filter_type_ = Type::NONE;
   int   channels_    = 1;
   int   sample_rate_ = 44100;
 
+  void
+  reset_biquad (BiquadState& state)
+  {
+    state.x1 = 0;
+    state.x2 = 0;
+    state.y1 = 0;
+    state.y2 = 0;
+  }
   float
   apply_hpf_1p (float in, float& tmp)
   {
@@ -62,7 +91,7 @@ private:
     float out = 0.5f * (in - p * x_h - tmp);
     tmp = x_h;
     return out;
-  };
+  }
   float
   apply_lpf_1p (float in, float& tmp)
   {
@@ -70,7 +99,17 @@ private:
     float out = 0.5f * (in + p * x_h + tmp);
     tmp = x_h;
     return out;
-  };
+  }
+  float
+  apply_biquad (float in, BiquadState& state)
+  {
+    float out = b0 * in + b1 * state.x1 + b2 * state.x2 - a1 * state.y1 - a2 * state.y2;
+    state.x2 = state.x1;
+    state.x1 = in;
+    state.y2 = state.y1;
+    state.y1 = out;
+    return out;
+  }
 public:
   void
   set_type (Type filter_type)
@@ -95,12 +134,27 @@ public:
         float t = tanf (M_PI * cutoff / sample_rate_);
         p = (t - 1) / (t + 1);
       }
+    else if (filter_type_ == Type::LPF_2P)
+      {
+        float k = tanf (M_PI * cutoff / sample_rate_);
+        float kk = k * k;
+        float q = 0.707;
+        float div_factor = 1  / (1 + (k + 1 / q) * k);
+
+        b0 = kk * div_factor;
+        b1 = 2 * kk * div_factor;
+        b2 = kk * div_factor;
+        a1 = 2 * (kk - 1) * div_factor;
+        a2 = (1 - k / q + kk) * div_factor;
+      }
   }
   void
   reset()
   {
     tmp_l = 0;
     tmp_r = 0;
+    reset_biquad (b_state_l);
+    reset_biquad (b_state_r);
   }
   void
   process (float *left, float *right, uint n_frames)
@@ -119,6 +173,14 @@ public:
           {
             left[i]  = apply_hpf_1p (left[i], tmp_l);
             right[i] = apply_hpf_1p (right[i], tmp_r);
+          }
+      }
+    if (filter_type_ == Type::LPF_2P)
+      {
+        for (uint i = 0; i < n_frames; i++)
+          {
+            left[i]  = apply_biquad (left[i], b_state_l);
+            right[i] = apply_biquad (right[i], b_state_r);
           }
       }
   }
