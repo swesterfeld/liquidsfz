4f913491f3bfbd0d590d4b90933e1da07f7f1c8a
Stefan Westerfeld <stefan@space.twc.de>
2021-01-08 18:03:48 +0100

    LIB: avoid non-portable vasprintf, use vsnprintf instead
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- lib/log.cc
+++ lib/log.cc
@@ -3,39 +3,39 @@
 #include <stdarg.h>
 
 #include <string>
+#include <vector>
 
 namespace LiquidSFZInternal
 {
 
 using std::string;
-
-static string
-string_vprintf (const char *format, va_list vargs)
-{
-  string s;
-
-  char *str = NULL;
-  if (vasprintf (&str, format, vargs) >= 0 && str)
-    {
-      s = str;
-      free (str);
-    }
-  else
-    s = format;
-
-  return s;
-}
+using std::vector;
 
 string
 string_printf (const char *format, ...)
 {
+  vector<char> buffer;
   va_list ap;
 
+  /* figure out required size */
   va_start (ap, format);
-  string s = string_vprintf (format, ap);
+  int size = vsnprintf (&buffer[0], buffer.size(), format, ap);
   va_end (ap);
 
-  return s;
+  if (size < 0)
+    return format;
+
+  /* now print with large enough buffer */
+  buffer.resize (size + 1);
+
+  va_start (ap, format);
+  size = vsnprintf (&buffer[0], buffer.size(), format, ap);
+  va_end (ap);
+
+  if (size < 0)
+    return format;
+
+  return &buffer[0];
 }
 
 }
