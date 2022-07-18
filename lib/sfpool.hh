b330ad4769abb02d641735a9bd796c0435888a6c
Stefan Westerfeld <stefan@space.twc.de>
2022-07-18 15:42:23 +0200

    LIB: change license to MPL-2.0
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- lib/sfpool.hh
+++ lib/sfpool.hh
@@ -1,22 +1,4 @@
-/*
- * liquidsfz - sfz sampler
- *
- * Copyright (C) 2022  Stefan Westerfeld
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
- * option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
- * for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 
 #ifndef LIQUIDSFZ_SFPOOL_HH
 #define LIQUIDSFZ_SFPOOL_HH
