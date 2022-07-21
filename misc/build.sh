532fabe275147132f01d8f25d292618173e09a10
Stefan Westerfeld <stefan@space.twc.de>
2022-07-21 13:38:18 +0200

    MISC: cleanup CI scripts
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ misc/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
+set -Eeuo pipefail
+
+########## Build / Test with gcc / ASAN / Debug C++
+CC=gcc CXX=g++
+$CXX --version
+./autogen.sh --enable-asan --enable-debug-cxx
+make -j `nproc` V=1
+make -j `nproc` check
+########## Cleanup
+
+make uninstall
+make distclean
+
+########## Build / Test with gcc / Debug C++
+./autogen.sh --enable-debug-cxx
+make -j `nproc` V=1
+make -j `nproc` check
+make install
+lv2lint http://spectmorph.org/plugins/liquidsfz
+
+########## Distcheck
+make -j `nproc` distcheck
+
+########## Cleanup
+make uninstall
+make distclean
+
+########## Build / Test with clang
+
+CC=clang CXX=clang++
+$CXX --version
+./autogen.sh
+make -j `nproc` V=1
+make -j `nproc` check
+make install
+lv2lint http://spectmorph.org/plugins/liquidsfz
