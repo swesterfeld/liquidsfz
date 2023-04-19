6bf28086751889d84285232c070dff84bcacea28
Stefan Westerfeld <stefan@space.twc.de>
2023-04-19 14:34:02 +0200

    WINDOWS: use config.sh from docker build
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- windows/build.sh
+++ windows/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -Eeo pipefail
 
-source static/config.sh
 docker build -f "windows/Dockerfile" -t liquidsfz-dbuild-win .
-docker run -v $PWD:/data -t liquidsfz-dbuild-win /bin/bash -c "
+docker run -v $PWD:/data -t liquidsfz-dbuild-win /bin/bash -c '
+  source static/config.sh
   cd /usr/local/liquidsfz-win/lib/lv2
-  zip -r /data/windows/liquidsfz-$PACKAGE_VERSION-win64.zip liquidsfz.lv2"
+  zip -r /data/windows/liquidsfz-$PACKAGE_VERSION-win64.zip liquidsfz.lv2'
