772676c9c6cd86559971ceff75ace7a9c0aff137
Stefan Westerfeld <stefan@space.twc.de>
2023-05-08 13:46:51 +0200

    MISC: include LV2 code in CI build
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- misc/macbuild.sh
+++ misc/macbuild.sh
@@ -4,6 +4,6 @@ set -Eeuo pipefail -x
 
 brew install autoconf-archive automake pkg-config libsndfile jack lv2 fftw
 export PKG_CONFIG_PATH="$(brew --prefix readline)/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
-./autogen.sh --without-lv2 --with-fftw
+./autogen.sh --with-fftw
 make -j `sysctl -n hw.ncpu`
 make check
