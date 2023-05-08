ee8c0e45127c7cac083bdf4bcd8fed7077c98a5f
Stefan Westerfeld <stefan@space.twc.de>
2023-05-08 10:55:33 +0200

    MISC: use make -j for parallel builds on macOS
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- misc/macbuild.sh
+++ misc/macbuild.sh
@@ -5,5 +5,5 @@ set -Eeuo pipefail -x
 brew install autoconf-archive automake pkg-config libsndfile jack lv2 fftw
 export PKG_CONFIG_PATH="$(brew --prefix readline)/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
 ./autogen.sh --without-lv2 --with-fftw
-make
+make -j `sysctl -n hw.ncpu`
 make check
