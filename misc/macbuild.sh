5d35885db2b918a0d36bdbaca5188a249f9c0626
Stefan Westerfeld <stefan@space.twc.de>
2024-06-09 10:45:12 +0200

    MISC: macbuild.sh: need libtool for CI on macos-latest
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- misc/macbuild.sh
+++ misc/macbuild.sh
@@ -2,7 +2,7 @@
 # This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 set -Eeuo pipefail -x
 
-brew install autoconf-archive automake pkg-config libsndfile jack lv2 fftw
+brew install autoconf-archive automake pkg-config libsndfile jack lv2 fftw libtool
 export PKG_CONFIG_PATH="$(brew --prefix readline)/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
 ./autogen.sh --with-fftw
 make -j `sysctl -n hw.ncpu`
