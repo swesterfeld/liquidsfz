41be77793397d611989a83dfab0593c31c4e35d1
Stefan Westerfeld <stefan@space.twc.de>
2026-03-07 12:57:33 +0100

    TESTS: testsynth: fix bad char in license
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tests/testsynth.cc
+++ tests/testsynth.cc
@@ -1,4 +1,4 @@
-// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0N
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 
 #include <cmath>
 #include <cstdio>
