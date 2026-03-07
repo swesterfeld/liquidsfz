0b654daf2a1ad407daeada2b05cf720078aafc52
Stefan Westerfeld <stefan@space.twc.de>
2026-03-07 14:54:38 +0100

    WINDOWS: disable lv2 plugin build for now (missing new style UI)
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- windows/build.sh
+++ windows/build.sh
@@ -2,7 +2,7 @@
 set -Eeo pipefail
 
 docker build -f "windows/Dockerfile" -t liquidsfz-dbuild-win .
-docker run -v $PWD:/data -t liquidsfz-dbuild-win /bin/bash -c '
-  source static/config.sh
-  cd /usr/local/liquidsfz-win/lib/lv2
-  zip -r /data/windows/liquidsfz-$PACKAGE_VERSION-win64.zip liquidsfz.lv2'
+#docker run -v $PWD:/data -t liquidsfz-dbuild-win /bin/bash -c '
+#  source static/config.sh
+#  cd /usr/local/liquidsfz-win/lib/lv2
+#  zip -r /data/windows/liquidsfz-$PACKAGE_VERSION-win64.zip liquidsfz.lv2'
