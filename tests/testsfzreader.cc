4c768786b08d2fb9fe6833c3846f69023359b5e9
Stefan Westerfeld <stefan@space.twc.de>
2026-04-04 14:36:33 +0200

    TESTS: testsfzreader: initial test for SfzReader
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

---
+++ tests/testsfzreader.cc
@@ -0,0 +1,25 @@
+#include "sfzreader.hh"
+#include "log.hh"
+#include <cassert>
+
+using namespace LiquidSFZInternal;
+
+int
+main()
+{
+  SfzReader sfz_reader;
+  string parsed;
+  string expected;
+  sfz_reader.on_opcode = [&] (const string& opcode, const string& value) { parsed += string_printf ("opcode: %s='%s'\n", opcode.c_str(), value.c_str()); };
+  sfz_reader.on_tag = [&] (const string& tag) { parsed += string_printf ("tag: %s\n", tag.c_str()); };
+
+  auto expect_tag = [&] (const string& tag) { expected += string_printf ("tag: %s\n", tag.c_str()); };
+  auto expect_opcode = [&] (const string& opcode, const string& value) {expected += string_printf ("opcode: %s='%s'\n", opcode.c_str(), value.c_str()); };
+  auto begin_test = [&] (const string& sfz) { parsed = ""; expected = ""; printf ("## parse: %s\n", sfz.c_str()); sfz_reader.parse (sfz); };
+  auto end_test = [&] () { printf ("%s\n", parsed.c_str()); assert (parsed == expected); };
+
+  begin_test ("<region>sample=*sine");
+  expect_tag ("<region>");
+  expect_opcode ("sample", "*sine");
+  end_test();
+}
